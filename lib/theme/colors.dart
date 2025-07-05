a2f7c8d673587d2828dac7b572f551e46eae96c7
alexanderfboyce2 <alexanderfboyce2@gmail.com>
2025-07-05 20:51:11 +0000

    changing colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -5,9 +5,9 @@ class AppColors {
 
   static const Color primaryDark = Color(0xFF011540);
   static const Color primaryBlue = Color(0xFF0196EC);
-  static const Color accentBlue1 = Color(0xFF08A8F6);
+  static const Color accentBlue1 = Color.fromARGB(255, 8, 83, 246);
   static const Color accentBlue2 = Color(0xFF0AC4FD);
-  static const Color primaryLight = Color(0xFFE3F2FD);
+  static const Color primaryLight = Color.fromARGB(255, 159, 173, 235);
   static const Color lightText = Color(0xFFFCFCFC);
   
 }
